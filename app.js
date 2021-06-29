be7a72b0ad17adc5fd4acde9dffdba9042fcbb80
kimchavsov <kimchavsov@gmail.com>
2021-06-29 11:28:54 +0800

    Add reset button

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const player = (name, marker) => {
   return {name, marker}
 };
 
-const gameController = (() => {
+const boardController = (() => {
   // Gameboard array
   const gameBoard = ["","","","","","","","",""];
 
@@ -17,51 +17,54 @@ const gameController = (() => {
     } 
     return false
   }
-  // Reset fucnction when reset button clicked
-  const reset = () => {
-    for (let block of gameBoard) {
-      block = ""
-    } 
+
+  // Function to reset gameBoard
+  const resetBoard = () => {
+    for (let i = 0; i < gameBoard.length; i++ ) {
+      gameBoard[i] = "";
+    }  
   }
+
   return {
     gameBoard,
     checkField,
-    reset
+    resetBoard
   }
 })()
 
-const Game = (() => {
+const gameController = (() => {
   const h3 = document.querySelector('h3');
 
-  const player1 = player('Player 1', 'X');
-  const player2 = player('Player 2', 'O');
+  const player1 = player('X', 'X');
+  const player2 = player('O', 'O');
 
   let turn = 1;
   let isOver = false;
 
+  // Check player and return player according to turn
   const checkPlayer = () => {
     return (turn % 2 === 1) ? player1: player2;
   }
 
+  // function is to play a turn and increment the turn as the game go
   const playTurn = (index) => {
     let player = checkPlayer();
-    gameController.checkField(index, player.marker);
-    console.log('Hel;lo' + checkWinner(index))
+    boardController.checkField(index, player.marker);
     if (checkWinner(index)) {
       isOver = true;
-      h3.textContent = `${Game.checkPlayer().name} WIN!!!!!!`
+      h3.textContent = `${gameController.checkPlayer().name} WIN!!!!!!`
       return;
     }
     else if (turn === 9) {
       isOver = true;
-      h3.innerHTML = 'Draw';
+      h3.innerHTML = 'It is a Draw';
       return;
     }
     turn++;
-    h3.innerHTML = `${Game.checkPlayer().name}'s turn`
+    h3.innerHTML = `${gameController.checkPlayer().name}'s turn`
   }; 
 
-  // Check Winner
+  // Check Winning condition return boolean value
   const checkWinner = (index) => {
     const winConditions = [
       [0,1,2],
@@ -73,39 +76,59 @@ const Game = (() => {
       [0,4,8],
       [2,4,6]
     ];
-
     return winConditions
       .filter((combination) => combination.includes(index))
       .some(
         (possibleCombination) => 
         possibleCombination.every(
-          (index) => gameController.gameBoard[index] === checkPlayer().marker)
+          (index) => boardController.gameBoard[index] === checkPlayer().marker)
       );
   };
 
+  // Function to return the over status
   const checkOver = () => {
-    console.log(isOver)
     return isOver;
   } 
+
+  // Reset function that restart the game
+  const reset = () => {
+    turn = 1;
+    boardController.resetBoard()
+    isOver = false;
+    h3.innerHTML = "X's Turn"
+  }
 
   return { 
     checkOver,
     checkPlayer,
-    playTurn, 
+    playTurn,
+    reset 
   }
 })()
 
+// Module to play the game
 const play = (() => {
   const board = Array.from(document.querySelectorAll('.block'));
+  const resetBtn = document.querySelector('#reset');
+
+  // Update the current board
+  const updateBoard = () => {
+    for (let i =  0; i < board.length; i++) {
+      board[i].textContent = boardController.gameBoard[i];
+    }
+  }
 
   // X and O on board 
   board.forEach(block => {block.addEventListener('click', (e) => {
-    if (Game.checkOver() || block.textContent !== "") {
-      return;
-    }
+    if (gameController.checkOver() || block.textContent !== "") {return}
     const index = board.indexOf(block)
-    Game.playTurn(index);
-    block.innerHTML = gameController.gameBoard[index]
-      // Game.checkOver()
+    gameController.playTurn(index);
+    updateBoard()
   })});
+
+  // Add reset butoon
+  resetBtn.addEventListener('click', () => {
+    gameController.reset()
+    updateBoard()
+  })
 })();
